efe2a2a7921239e40f199ea9643b896ec156ba95
Andreas Stolpmann <a.stolpmann@gmail.com>
2019-07-21 12:39:00 +0200

    Improved

---
+++ CSharp8/NullCoalescingAssignmentOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NullCoalescingAssignmentOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(null);
+        }
+
+        static void Print(string s)
+        {
+            // s = s ?? "NotNull!"
+
+            s ??= "NotNull!";
+
+            Console.WriteLine(s);
+        }
+
+    }
+}
--- CSharp8/NullableReferences/Program.cs
+++ CSharp8/NullableReferences/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+#nullable enable
 namespace NullableReferences
 {
     public class Program
@@ -7,6 +8,33 @@ namespace NullableReferences
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int GetNameLength(Person person)
+        {
+            person.MiddleName ??= "asdi";
+
+            return person.FirstName.Length + person.MiddleName.Length + person.LastName.Length;
+        }
+
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public Person(string firstName, string middleName, string lastName)
+        {
+            FirstName = firstName;
+            MiddleName = middleName;
+            LastName = lastName;
         }
     }
 }
